4b10ab9d4da1a3748bbe60b9363a64ce7f0b5474
James Tooze <toozej@gmail.com>
2023-06-19 16:40:54 -0700

    revamp to also work with SomaFM, and restart daily

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
-# rff-auto-player
+# auto-radio-player
 
 
 ## Intro
-Radio Free Fedi Auto Player is a quick and simple script to install a SystemD service unit for automatically playing the [Radio Free Fedi "Comfy" mp3 stream](https://radiofreefedi.net/) on system startup.
+Auto Radio Player is a quick and simple script to install a SystemD service unit for automatically playing a variety of Internet radio stations on system startup and rotating daily.
 
-In particular this service was designed to be ran headlessly on a Raspberry Pi running a Debian-based distro, and outputting audio to the 3.5mm aux jack. And since it is designed to be ran headlessly, automatic unattended security updates are also enabled.
+In particular this service was designed to be ran headlessly on a Raspberry Pi running a Debian-based distro, and outputting audio to the 3.5mm aux jack. And since it is designed to be ran headlessly, automatic unattended updates with automatic reboots are also enabled.
+
+Radio stations pre-installed:
+- [Radio Free Fedi "Comfy" mp3 stream](https://radiofreefedi.net/)
+- [SomaFM stations in highest AAC quality](https://somafm.com/listen/)
 
 If one so desired, this setup could be adjusted to play any other audio file or stream by passing an argument ${1} to the `install.sh` script.
 
 
 ## Pre-reqs
-- Some kind of Debian-based distro using SystemD
+- Some kind of Debian-based distro using SystemD (Raspberry Pi OS works great on RPi)
 - `bash` and `curl`
 - `sudo` or root permissions
 
@@ -19,16 +23,16 @@ If one so desired, this setup could be adjusted to play any other audio file or 
 
 One-liner if you like to live dangerously:
 ```bash
-curl -s -L https://github.com/toozej/rff-auto-player/blob/main/install.sh | sudo bash
+curl -s -L https://github.com/toozej/auto-radio-player/blob/main/install.sh | sudo bash
 ```
 
 If you'd like to use a different radio station (or file):
 ```bash
-curl -s -L https://github.com/toozej/rff-auto-player/blob/main/install.sh | sudo bash -s AUDIO_FILE_OR_STREAM_GOES_HERE
+curl -s -L https://github.com/toozej/auto-radio-player/blob/main/install.sh | sudo bash -s AUDIO_FILE_OR_STREAM_GOES_HERE
 ```
 
 If you prefer git:
 ```bash
-git clone git@github.com:toozej/rff-auto-player.git
-sudo ./rff-auto-player/install.sh
+git clone git@github.com:toozej/auto-radio-player.git
+sudo ./auto-radio-player/install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,39 +1,72 @@
 #!/usr/bin/env bash
 
-# simple script to setup a Radio Free Fedi streamer on a Raspberry Pi or similar
+# simple script to setup an automatic radio streamer on a Raspberry Pi or similar
 
 echo "updating and installing required packages"
 # update to latest packages all around
 apt-get update -qq && apt-get upgrade -y
 
 # install mplayer and unattended-upgrades packages
-apt-get install -y mplayer unattended-upgrades
+apt-get install -y mplayer unattended-upgrades curl
 
 
-# set Radio Free Fedi mp3 stream URL:
-echo "setting up Radio Free Fedi stream URL"
+# set radio stream URLs list:
+echo "setting up list of radio stream URLs"
+radio_conf="/etc/radio.conf"
+rm -f "${radio_conf}" && touch "${radio_conf}"
+chmod 0644 "${radio_conf}"
 if [ -n "${1}" ]; then
-    radio_url="${1}"
+    for arg in "$@"; do
+        echo "${arg}" >> "${radio_conf}"
+    done
 else
-    radio_url="http://217.20.116.68:8055/comfy"
-fi
-echo "RFF stream URL set to: ${radio_url}"
+    # append SomaFM music channels
+    somafm_channels=$(curl -s -H 'Accept: application/json' https://somafm.com/channels.json | jq -r ".channels | map(select(.genre != \"spoken\" and (.genre | test(\"news\"; \"i\") | not))) | .[]" | jq -r ".playlists | map(select(.quality == \"highest\" and .format == \"aac\")) | limit(1;.[]) | .url")
+    while IFS= read -r line; do
+        echo "${line}" >> "${radio_conf}"
+    done <<< "${somafm_channels}"
 
-# setup RFF SystemD service
-echo "setting up RFF radio SystemD service"
+    # append Radio Free Fedi
+    echo "http://217.20.116.68:8055/comfy" >> ${radio_conf}
+fi
+echo "Radio stream URLs set to: $(cat /etc/radio.conf)"
+
+
+# setup script to randomly select radio stream and start playback
+curl -O /usr/local/bin/radio --location --remote-header-name --remote-name https://raw.githubusercontent.com/toozej/auto-radio-player/main/radio
+chmod 0775 /usr/local/bin/radio
+
+
+# setup SystemD service
+echo "setting up auto radio SystemD service"
 tee > /etc/systemd/system/radio.service << EOF
 [Unit]
-Description=Radio Free Fedi service
+Description=Auto Radio Player service
 
 [Service]
 ExecStartPre=/usr/bin/sleep 15
-ExecStart=/usr/bin/mplayer -quiet ${radio_url}
+ExecStart=/usr/local/bin/radio
+Restart=always
+RestartSec=5s
 
 [Install]
 WantedBy=multi-user.target
 EOF
+chmod 0664 /etc/systemd/system/radio.service
 
-chmod 664 /etc/systemd/system/radio.service
+# setup SystemD service timer
+tee > /etc/systemd/system/radio.timer << EOF
+[Unit]
+Description=Timer for radio service
+
+[Timer]
+OnCalendar=daily
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+chmod 0664 /etc/systemd/system/radio.timer
 systemctl daemon-reload
 systemctl enable radio.service
 systemctl start radio.service
@@ -41,19 +74,27 @@ systemctl start radio.service
 
 # set volume level
 echo "setting volume level"
-amixer -M sset PCM 80%
+amixer -M sset PCM 90%
 
 
 # Configure automatic security updates
 echo "setting unattended security updates"
 tee > /etc/apt/apt.conf.d/50unattended-upgrades <<EOF
-Unattended-Upgrade::Allowed-Origins {
+Unattended-Upgrade::Origins-Patterns {
+    "\${distro_id}:\${distro_codename}";
+    "\${distro_id}:\${distro_codename}-updates";
     "\${distro_id}:\${distro_codename}-security";
 };
 Unattended-Upgrade::Package-Blacklist {
 };
-Unattended-Upgrade::MinimalSteps "true";
-Unattended-Upgrade::Automatic-Reboot "false";
+Unattended-Upgrade::MinimalSteps "false";
+Unattended-Upgrade::Remove-Unused-Dependencies "true";
+Unattended-Upgrade::Automatic-Reboot "true";
+Unattended-Upgrade::Automatic-Reboot-Time "03:00";
+EOF
+tee > /etc/apt/apt.conf.d/20auto-upgrades <<EOF
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
 EOF
 
 echo "all done!"
